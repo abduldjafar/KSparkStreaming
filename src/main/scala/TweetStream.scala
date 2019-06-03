a23c06b08ef6132d5ce96441c383cdb1021ab7bb
abdul <abdul.haris@dattabot.io>
2019-06-04 05:01:03 +0700

    Add neew Files

--- src/main/scala/TweetStream.scala
+++ src/main/scala/TweetStream.scala
@@ -8,60 +8,6 @@ import org.apache.spark.sql.{Column, DataFrame, Row}
 
 
 object TweetStream{
-  // function for regex findall
-  def regexp_extractAll = udf((job: String, exp: String, groupIdx: Int) => {
-    val pattern = Pattern.compile(exp.toString)
-    val m = pattern.matcher(job.toString)
-    var result = Seq[String]()
-    while (m.find) {
-      val temp =
-        result =result:+m.group(groupIdx)
-    }
-    result.toArray
-  })
-
-  // function for check string
-  def parseToInt = udf((entry: String) => {
-    try {
-      entry.toInt
-    } catch {
-      case e: Exception => 0
-    }
-  })
-
-  def cleansingTweet = udf((entry: String) => {
-    // Remove HTML special entities (e.g. &amp;)
-    val regexhtml ="\\&\\w*;".r
-    val result1 = regexhtml.replaceAllIn(entry,"")
-
-    //remove @username
-    val regexAd = "@[^\\s]+".r
-    val result2 = regexAd.replaceAllIn(result1,"")
-
-    //remove RT
-    val regexRT = "rt".r
-    val result3 = regexRT.replaceAllIn(result2,"")
-
-    //remove tickers
-    val regextTicks = "\\$\\w*".r
-    val result4 = regextTicks.replaceAllIn(result3,"")
-
-    //convert to lower
-    val result5 = result4.toLowerCase()
-
-    //remove hyperlinks
-    val regexHyper = "https?:\\/\\/.*\\/\\w*".r
-    val result6 = regexHyper.replaceAllIn(result5,"")
-
-    //remove hashtag
-    val regexHastag = "#\\w*".r
-    val result7 = regexHastag.replaceAllIn(result6,"")
-
-    result7.trim()
-
-  }
-  )
-
   def main(args: Array[String]): Unit = {
     val spark = builder.master("local").appName("Spark Sentyment Analysyst").getOrCreate
 
