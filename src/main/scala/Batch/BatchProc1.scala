f946fb15551324be86e8b30b061fe281e172dcce
abdul <abdul.haris@dattabot.io>
2019-07-30 05:21:07 +0700

    Add neew Files

---
+++ src/main/scala/Batch/BatchProc1.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.sql.SparkSession.builder
+import org.apache.spark.sql.functions._
+
+object BatchProc1{
+  def main(args: Array[String]): Unit = {
+    val spark = builder.master("local").appName("Batch Proces").getOrCreate
+    val dfraw = spark.read.format("csv").option("inferSchema", "true").option("header","true").load("USvideos.csv")
+
+    // get most views
+    val mostviews = dfraw.filter("views > 100000")
+      .sort(desc("views"))
+      mostviews.printSchema()
+
+  }
+}
