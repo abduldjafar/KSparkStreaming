1ee0ef475b56d9915d556c5c1c82270bd56c8485
kotekaman <abdul.haris.djafar@gmail.com>
2019-10-29 21:38:17 +0700

    add read json processing

--- build.sbt
+++ build.sbt
@@ -2,14 +2,19 @@ name := "KSparkStreaming"
 
 version := "0.1"
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.12.10"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.2"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.2"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.2" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.4" % "provided"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10
 libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.0" % "provided"
 // https://mvnrepository.com/artifact/mysql/mysql-connector-java
 libraryDependencies += "mysql" % "mysql-connector-java" % "8.0.16"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.4" % "runtime"
+// https://mvnrepository.com/artifact/org.postgresql/postgresql
+libraryDependencies += "org.postgresql" % "postgresql" % "9.4.1207.jre7"
+
---
+++ src/main/scala/Batch/ReadJson.scala
@@ -0,0 +1,36 @@
+package Batch
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.SparkSession.builder
+import org.apache.spark.sql.functions._
+
+
+object ReadJson {
+  def main(args: Array[String]): Unit = {
+    val spark = builder.master("local").appName("Batch Proces").getOrCreate
+    val path = "/Users/abdulharisdjafar/Documents/code/datasets/netzme/data*"
+    val peopleDF = spark.read.format("csv").option("inferSchema", "true").option("header","true").load(path)
+
+    peopleDF.select("{country_subdivision}","{revenue}","{event_name}","{last_session_time}")
+      .withColumnRenamed("{country_subdivision}","provinsi")
+      .withColumnRenamed("{revenue}","revenue")
+      .withColumnRenamed("{event_name}","event_name")
+      .withColumnRenamed("{last_session_time}","last_session_time")
+      .filter("revenue is not null").filter("provinsi is not null")
+      .filter("last_session_time is not null").filter("event_name is not null")
+      .withColumn("last_session_time",from_unixtime(new Column("last_session_time")))
+      .withColumn("year",year(new Column("last_session_time")))
+      .withColumn("month",month(new Column("last_session_time")))
+      .groupBy("provinsi","year").pivot("month").sum("revenue")
+      .na.fill(0)
+      .write
+      .format("jdbc")
+      .option("url", "jdbc:postgresql:postgres")
+      .option("dbtable", "test")
+      .option("user", "postgres")
+      .option("password", "toor")
+      .save()
+
+
+
+  }
+}
